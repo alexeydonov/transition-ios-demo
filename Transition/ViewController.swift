5129147ec68f1a204fef7b9e8d3692fdfbdea677
Alexey Donov <alexeydonov@me.com>
2018-11-15 16:17:51 +0300

    Initial Commit

---
+++ Transition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Transition
+//
+//  Created by Alexey Donov on 15.11.2018.
+//  Copyright © 2018 Alexey Donov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
